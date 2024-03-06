4c1e6773453a5cda0f909fdc35d869f3b7614a26
M. Feyza Yıldırım <144602340+Mfeyza@users.noreply.github.com>
2024-03-07 00:16:46 +0300

    app.js-completed

--- app.js
+++ app.js
@@ -4,3 +4,15 @@
 /* -------------------------------------------------------------------------- */
 /*                               BOOK ASSIGNMENT                              */
 /* -------------------------------------------------------------------------- */
+
+const express=require('express')
+const app=express()
+
+require('dotenv').config();
+const PORT=process.env.PORT || 8000;
+
+require ('express-async-errors')
+
+app.use(express.json())
+
+app.listen(PORT, ()=> console.log('runned : http://127.0.0.1:' +PORT))
---
+++ app/models/book.model.js
@@ -0,0 +1,8 @@
+'use strict'
+
+/* -------------------------------------------------------------------------- */
+/*                                 BOOK MODEL                                 */
+/* -------------------------------------------------------------------------- */
+
+const {Sequelize,DataTypes}=require('sequelize')
+const sequelize=new Sequelize
